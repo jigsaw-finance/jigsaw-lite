a32da2e4428635056783c8dbe6ca2477e3418482
Hovooo <avakiandev@gmail.com>
2024-03-28 13:34:00 +0800

    Improved Holding Contract
        - Implemented OZ's ReentrancyGuard
        - Added new functionality:
            - Unstake to  withdraw a specified amount of tokens to a designated
            address from Ion Pool
            - GenericCall to execute a generic call to interact with external
            contracts.
        - Updated errors

--- src/Holding.sol
+++ src/Holding.sol
@@ -2,30 +2,85 @@
 pragma solidity ^0.8.0;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 import { IHolding } from "./interfaces/IHolding.sol";
+import { IIonPool } from "./interfaces/IIonPool.sol";
 
-contract Holding is IHolding {
+contract Holding is IHolding, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    /// @notice returns the StakingManager address
-    address public override stakingManager;
+    /**
+     * @dev Address of the Staking Manager contract.
+     */
+    address public stakingManager;
 
-    /// @notice indicates if the contract has been initialized
+    /**
+     * @dev Address of the Ion Pool contract.
+     */
+    address public ionPool;
+
+    /**
+     * @dev Indicates if the contract has been initialized.
+     */
     bool private _initialized;
 
-    /// @notice this function initializes the contract (instead of a constructor) to be cloned
-    /// @param _stakingManager contract handler for staking
-    function init(address _stakingManager) public {
+    // --- Modifiers ---
+
+    modifier onlyStakingManager() {
+        if (msg.sender == stakingManager) revert UnauthorizedCaller();
+        _;
+    }
+
+    // --- Initialization ---
+
+    /**
+     * @dev Initializes the contract (instead of a constructor) to be cloned.
+     * @param _stakingManager The address of the contract handling staking operations.
+     */
+    function init(address _stakingManager, address _ionPool) external {
         require(!_initialized, "");
         require(_stakingManager != address(0), "");
         _initialized = true;
         stakingManager = _stakingManager;
+        ionPool = _ionPool;
     }
 
-    modifier onlyStakingManager() {
-        require(msg.sender == stakingManager, "1000");
-        _;
+    // -- Staker's operations  --
+
+    /**
+     * @notice Allows to withdraw a specified amount of tokens to a designated address from Ion Pool.
+     * @dev Only accessible by the staking manager and protected against reentrancy.
+     *
+     * @param _to Address to which the redeemed underlying asset should be sent to.
+     * @param _amount of underlying to redeem for.
+     */
+    function unstake(address _to, uint256 _amount) external onlyStakingManager nonReentrant {
+        IIonPool(ionPool).withdraw(_to, _amount);
+        emit Unstaked(msg.sender, _amount);
+    }
+
+    /**
+     * @notice Executes a generic call to interact with another contract.
+     * @dev This function is restricted to be called only by Staking Manager contract
+     * aimed at mitigating potential risks associated with unauthorized calls.
+     *
+     * @param _contract The address of the target contract for the call.
+     * @param _call ABI-encoded data representing the call to be made.
+     *
+     * @return success A boolean indicating whether the call was successful or not.
+     * @return result The result of the call as bytes.
+     */
+    function genericCall(
+        address _contract,
+        bytes calldata _call
+    )
+        external
+        onlyStakingManager
+        nonReentrant
+        returns (bool success, bytes memory result)
+    {
+        (success, result) = _contract.call(_call);
     }
 }
