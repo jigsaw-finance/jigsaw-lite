11bdbb101700aafd2ae340e7137d5f09561a4aab
Hovooo <avakiandev@gmail.com>
2024-04-09 12:10:46 +0800

    Implemented contracts' interface validation
    Implemented contracts' interface validation in StakingManager's deployment
    script.

--- script/deploy/DeployStakingManager.s.sol
+++ script/deploy/DeployStakingManager.s.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.20;
 
 import { stdJson as StdJson } from "forge-std/Script.sol";
 
-import "./Base.s.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IIonPool } from "../../test/utils/IIonPool.sol";
+
+import { BaseScript } from "./Base.s.sol";
 import { StakingManager } from "../../src/StakingManager.sol";
 
 contract DeployStakingManagerScript is BaseScript {
@@ -19,6 +22,10 @@ contract DeployStakingManagerScript is BaseScript {
     uint256 REWARDS_DURATION = config.readUint(".rewardsDuration");
 
     function run() external broadcast returns (StakingManager stakingManager) {
+        _validateInterface(IIonPool(ION_POOL));
+        _validateInterface(IERC20(UNDERLYING));
+        _validateInterface(IERC20(REWARD_TOKEN));
+
         stakingManager = new StakingManager({
             _admin: DEFAULT_ADMIN,
             _underlyingAsset: UNDERLYING,
