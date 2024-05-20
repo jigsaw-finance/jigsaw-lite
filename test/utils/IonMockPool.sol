82a46c7a259eb1be43e59d587346a5d65a93fad7
Hovooo <avakiandev@gmail.com>
2024-05-20 16:26:09 +0700

    Enhanced Ion Pool Mock
    Extended the functionality of the Ion Pool mock contract to provide a
    better testnet testing experience for the front-end and other team
    members.

--- test/utils/IonMockPool.sol
+++ test/utils/IonMockPool.sol
@@ -1,13 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 //Ion Pool Mock for testing deployment and interface verification
 contract IonPool {
-    uint256 val;
-    address addr;
+    using SafeERC20 for IERC20;
 
-    function balanceOf(address) external view returns (uint256) {
-        return val;
+    address immutable underlyingAsset;
+    address immutable addr;
+    uint256 immutable val;
+
+    mapping(address user => uint256 balance) userBalance;
+
+    constructor(address _underlyingAsset) {
+        underlyingAsset = _underlyingAsset;
+    }
+
+    function balanceOf(address user) external view returns (uint256) {
+        return userBalance[user];
     }
 
     function debt() external view returns (uint256) {
@@ -18,5 +30,13 @@ contract IonPool {
         return addr;
     }
 
-    function supply(address user, uint256 amount, bytes32[] calldata proof) external { }
+    function supply(address user, uint256 amount, bytes32[] calldata) external {
+        userBalance[user] += amount;
+        IERC20(underlyingAsset).safeTransferFrom({ from: msg.sender, to: address(this), value: amount });
+    }
+
+    function withdraw(address to, uint256 amount) external {
+        userBalance[msg.sender] -= amount;
+        IERC20(underlyingAsset).safeTransfer({ to: to, value: amount });
+    }
 }
